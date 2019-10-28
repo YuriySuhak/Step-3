95654ea396bbf133295915431cac9fe068ed5401
Eugen Markov <seroglaziymail@gmail.com>
2019-10-28 21:07:51 +0200

    change for Visit class

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 
 class Visit {
-    constructor(client, doctor, goal, status, urgency) {
-        // this.id = "";
+    constructor(client = null, doctor = null, goal = null, status = null, urgency = null, summary = null, doctorName = null, id = null) {
+        this.id = id;
         this.client = client;
-        this.doctor = doctor;
         this.goal = goal;
         this.status = status;
         this.urgency = urgency;
+        this.doctor = doctor;
+        this.doctorName = doctorName;
+        this.summary = summary;
     }
-    static createInputs(form) {
+    createInputs(form) {
         form.innerHTML = "";
 
         const nameInput = document.createElement("input");
-        nameInput.name= "name";
+        nameInput.value = this.client;
+        nameInput.name = "name";
         nameInput.id = "clientName";
         nameInput.placeholder = "ФИО";
         nameInput.setAttribute("required", "true");
         // nameInput.classList.add("new-visit-input");
+
         const nameInputLabel = document.createElement("label");
-        nameInputLabel.innerText = "Пациент";
+        nameInputLabel.innerText = "Пациент: ";
         nameInputLabel.append(nameInput);
 
         const goalInput = document.createElement("input");
+        goalInput.value = this.goal;
         goalInput.name = "goal";
         goalInput.id = "goalId";
         goalInput.placeholder = "Обследование";
         goalInput.setAttribute("required", "true");
         const goalInputLabel = document.createElement("label");
-        goalInputLabel.innerText = "Цель визита";
+        goalInputLabel.innerText = "Цель визита: ";
         goalInputLabel.append(goalInput);
 
         const urgencySelect = document.createElement("select");
+        urgencySelect.value = this.urgency;
         urgencySelect.name = "urgency";
         urgencySelect.setAttribute("required", "true");
         const urgencyLevelHigh = document.createElement("option");
@@ -47,6 +53,7 @@ class Visit {
         const statusSelect = document.createElement("select");
 
         statusSelect.setAttribute("required", "true");
+        statusSelect.value = this.status;
         statusSelect.name = "status";
         const statusOpen = document.createElement("option");
         statusOpen.innerText = "Открыт";
@@ -59,11 +66,12 @@ class Visit {
 
 
         const summaryInput = document.createElement("textarea");
+        summaryInput.value = this.summary;
         summaryInput.name = "summary";
         summaryInput.id = "summaryId";
         summaryInput.placeholder = "Пациента беспокоит...";
         const summaryInputLabel = document.createElement("label");
-        summaryInputLabel.innerText = "Краткое описание";
+        summaryInputLabel.innerText = "Краткое описание: ";
         summaryInputLabel.append(summaryInput);
 
         form.append(nameInputLabel, goalInputLabel, urgencySelectLabel,  statusSelectLabel, summaryInputLabel);
@@ -71,19 +79,21 @@ class Visit {
 }
 
 class VisitToDentist extends Visit {
-    constructor(lastVisitDate,...args){
+    constructor(lastVisitDate = "",...args){
         super(...args);
         this.lastVisitDate = lastVisitDate;
+        this.content = {name: this.client, lastVisitDate: this.lastVisitDate, doctorName: this.doctorName};
     }
-    static createInputs(form) {
+    createInputs(form) {
         super.createInputs(form);
         const lastVisitDateInput = document.createElement("input");
+        lastVisitDateInput.value = this.content.lastVisitDate;
         lastVisitDateInput.name = "lastVisit";
         lastVisitDateInput.id = "lastVisitId";
         lastVisitDateInput.placeholder = "01.01.2019";
         lastVisitDateInput.setAttribute("required", "true");
         const lastVisitDateInputLabel = document.createElement("label");
-        lastVisitDateInputLabel.innerText = "Дата последнего посещения";
+        lastVisitDateInputLabel.innerText = "Дата последнего посещения: ";
         lastVisitDateInputLabel.append(lastVisitDateInput);
 
         const dentists = document.createElement("select");
@@ -102,52 +112,77 @@ class VisitToDentist extends Visit {
 
         form.append(lastVisitDateInputLabel, dentistsLabel);
     }
+    submitForm() {
+        $("#new-card").on("submit", function(e){
+            e.preventDefault();
+            const data = ($(this).serializeArray());
+            console.log(data);
+            const visit = new VisitToDentist(data[6].value, data[1].value, data[0].value, data[2].value, data[4].value, data[3].value, data[5].value, data[7].value,)
+            console.log(visit);
+            const object = {
+                doctor: visit.doctor,
+                title: visit.goal,
+                description: visit.summary,
+                status: visit.status,
+                priority: visit.urgency,
+                content: JSON.stringify(visit.content)
+            };
+            cards.push(object);
+            console.log(cards);
+        })
+
+    }
 }
 
 class VisitToCardiologist extends Visit {
-    constructor(pressure, weightIndex, illness, age,...args){
+    constructor(pressure = "", weightIndex ="", illness = "", age = "",...args){
         super(...args);
         this.pressure = pressure;
         this.weightIndex = weightIndex;
         this.illness = illness;
         this.age = age;
+        this.content = {name: this.client, pressure: this.pressure, weightIndex: this.weightIndex, illness: this.illness, age: this.age, doctorName: this.doctorName};
     }
-    static createInputs(form) {
+    createInputs(form) {
         super.createInputs(form);
         const pressureInput = document.createElement("input");
+        pressureInput.value = this.pressure;
         pressureInput.name = "pressure";
         pressureInput.id = "pressureId";
         pressureInput.setAttribute("required", "true");
-        pressureInput.placeholder = "...";
+        pressureInput.placeholder = "../..";
         const pressureInputLabel = document.createElement("label");
-        pressureInputLabel.innerText = "Обычное давление";
+        pressureInputLabel.innerText = "Обычное давление: ";
         pressureInputLabel.append(pressureInput);
 
         const illnessInput = document.createElement("input");
+        illnessInput.value = this.illness;
         illnessInput.name = "illness";
         illnessInput.id = "illnessId";
         illnessInput.setAttribute("required", "true");
         illnessInput.placeholder = "...";
         const illnessInputLabel = document.createElement("label");
-        illnessInputLabel.innerText = "Перенесенные заболевания";
+        illnessInputLabel.innerText = "Перенесенные заболевания: ";
         illnessInputLabel.append(illnessInput);
 
         const weightIndexInput = document.createElement("input");
+        weightIndexInput.value = this.weightIndex;
         weightIndexInput.name = "weightIndex";
         weightIndexInput.id = "weightIndexId";
         weightIndexInput.type = "number";
         weightIndexInput.placeholder = "25";
         const  weightIndexInputLabel = document.createElement("label");
-        weightIndexInputLabel.innerText = "Индекс массы тела";
+        weightIndexInputLabel.innerText = "Индекс массы тела: ";
         weightIndexInputLabel.append(weightIndexInput);
 
         const ageInput = document.createElement("input");
+        ageInput.value = this.age;
         ageInput.name = "age";
         ageInput.id = "clientAgeId";
         ageInput.type = "number";
         ageInput.placeholder = "30";
         const ageInputLabel = document.createElement("label");
-        ageInputLabel.innerText = "Возраст пациента";
+        ageInputLabel.innerText = "Возраст пациента: ";
         ageInputLabel.append(ageInput);
 
         const cardiologists = document.createElement("select");
@@ -166,22 +201,43 @@ class VisitToCardiologist extends Visit {
 
         form.append(pressureInputLabel, illnessInputLabel, weightIndexInputLabel, ageInputLabel, cardiologistsLabel);
     }
+    submitForm(){
+        $("#new-card").on("submit", function(e){
+            e.preventDefault();
+            const data = ($(this).serializeArray());
+            console.log(data);
+            const visit = new VisitToCardiologist(data[6].value, data[8].value, data[7].value, data[9].value, data[1].value, data[0].value, data[2].value, data[4].value, data[3].value, data[5].value, data[10].value)
+            console.log(visit);
+            const object = {
+                doctor: visit.doctor,
+                title: visit.goal,
+                description: visit.summary,
+                status: visit.status,
+                priority: visit.urgency,
+                content: JSON.stringify(visit.content)
+            };
+            cards.push(object);
+            console.log(cards);
+        })
+    }
 }
 
 class VisitToTherapist extends Visit {
     constructor(age,...args){
         super(...args);
         this.age = age;
+        this.content = {name: this.client, age: this.age, doctorName: this.doctorName};
     }
-    static createInputs(form) {
+    createInputs(form) {
         super.createInputs(form);
         const ageInput = document.createElement("input");
+        ageInput.value = this.age;
         ageInput.name = "age";
         ageInput.id = "ageId";
         ageInput.type = "number";
         ageInput.placeholder = "30";
         const ageInputLabel = document.createElement("label");
-        ageInputLabel.innerText = "Возраст пациента";
+        ageInputLabel.innerText = "Возраст пациента: ";
         ageInputLabel.append(ageInput);
 
         const therapists = document.createElement("select");
@@ -198,6 +254,26 @@ class VisitToTherapist extends Visit {
 
         form.append(ageInputLabel, therapistsLabel);
     }
+
+    submitForm(){
+        $("#new-card").on("submit", function(e){
+            e.preventDefault();
+            const data = ($(this).serializeArray());
+            console.log(data);
+            const visit = new VisitToTherapist(data[6].value, data[1].value, data[0].value, data[2].value, data[4].value, data[3].value, data[5].value, data[7].value,)
+            console.log(visit);
+            const object = {
+                doctor: visit.doctor,
+                title: visit.goal,
+                description: visit.summary,
+                status: visit.status,
+                priority: visit.urgency,
+                content: JSON.stringify(visit.content)
+            };
+            cards.push(object);
+            console.log(cards);
+        })
+    }
 }
 
 const optionalInputs = document.getElementById("optionalInputs");
@@ -205,32 +281,15 @@ const optionalInputs = document.getElementById("optionalInputs");
 const selectedDoctor = document.getElementById("selectDoctor");
 selectedDoctor.addEventListener("change", (e)=> {
     if (e.currentTarget.value === "cardiologist") {
-        VisitToCardiologist.createInputs(optionalInputs);
+        new VisitToCardiologist().createInputs(optionalInputs);
+        new VisitToCardiologist().submitForm() ;
     } else if (e.currentTarget.value === "dentist"){
-        VisitToDentist.createInputs(optionalInputs);
+        new VisitToDentist().createInputs(optionalInputs);
+        new VisitToDentist().submitForm();
     } else if (e.currentTarget.value === "therapist"){
-        VisitToTherapist.createInputs(optionalInputs);
+        new VisitToTherapist().createInputs(optionalInputs);
+        new VisitToTherapist().submitForm();
     }
 });
 
-const creatNewCardBtn = document.getElementById("create-new-card-btn");
-const newCard = document.getElementById("new-card");
-
-
-$("#new-card").on("submit", function(e){
-    e.preventDefault();
-    const data = ($(this).serializeArray());
-    console.log(data);
-    if (selectedDoctor.value === "cardiologist") {
-        const visit = new VisitToCardiologist(data[6].value, data[8].value, data[7].value, data[9].value, data[1].value, data[10].value, data[2].value,  data[4].value, data[3].value)
-        console.log(visit);
-    };
-    if (selectedDoctor.value === "dentist") {
-        const visit = new VisitToDentist(data[6].value, data[1].value, data[7].value, data[2].value,  data[4].value, data[3].value)
-        console.log(visit);
-    };
-    if (selectedDoctor.value === "therapist") {
-        const visit = new VisitToTherapist(data[6].value, data[1].value, data[7].value, data[2].value,  data[4].value, data[3].value)
-        console.log(visit);
-    }
-})
+const cards = [];
