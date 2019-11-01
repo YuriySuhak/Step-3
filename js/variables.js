114726c4ca8eaf00498de46faffc0518a730a1b5
Eugen Markov <seroglaziymail@gmail.com>
2019-11-01 20:06:07 +0200

    clean useless code

--- js/variables.js
+++ js/variables.js
@@ -1,30 +1,5 @@
 let authToken;
 let authConfig = {};
-let cards = [
-    // {content: {name: "Тарнавська", lastVisitDate: "01.01.2019", doctorName: "Иванов А.И."},
-    //     description: "боли",
-    //     doctor: "dentist",
-    //     id: 1,
-    //     priority: "Высокая",
-    //     status: "Открыт",
-    //     title: "Обследование"
-    // },
-    // {content: {name: "Цабекало", pressure: "120/60", weightIndex: "31", illness: "грипп", age: "66", doctorName: "Петренко А.И."},
-    //     description: "Высокое давление",
-    //     doctor: "cardiologist",
-    //     id: 2,
-    //     priority: "Средняя",
-    //     status: "Открыт",
-    //     title: "Плановое обследование"
-    // },
-    // {content: {name: "Онищенко", age: "44", doctorName: "Мурило С.И."},
-    //     description: "недомогание",
-    //     doctor: "therapist",
-    //     id: 3,
-    //     priority: "Низкая",
-    //     status: "Открыт",
-    //     title: "Обследование",
-    // }
-];
+let cards = [];
 
 
